e95a2cc50c73a7ba59f4282c984fbd1a7415874d
Gowsigan A <e18115@eng.pdn.ac.lk>
2023-10-10 20:59:14 +0530

    Define ENV variables.

--- src/test/java/com/british_ariways/services/BritishAirwaysApplicationTests.java
+++ src/test/java/com/british_ariways/services/BritishAirwaysApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BritishAirwaysApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
